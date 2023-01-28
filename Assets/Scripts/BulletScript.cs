dcb08464932ca72b96fb228d2eb30699b5d1864a
ituitis-altintasb18 <56095106+ituitis-altintasb18@users.noreply.github.com>
2023-01-29 00:22:22 +0300

    Enemy now Dies and bullet disappears too

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -27,5 +27,13 @@ public class BulletScript : MonoBehaviour
     {
         Destroy(gameObject, 2f);
     }
-
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
 }
